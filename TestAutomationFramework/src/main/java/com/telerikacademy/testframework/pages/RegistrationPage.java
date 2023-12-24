c53737cb0c978cf46f965700b81c9a553cc98e15
Yordan Nikolov <x-normalize@protonmail.com>
2023-12-24 14:28:22 +0200

    Update RegistrationPage and remove duplicated code.

--- TestAutomationFramework/src/main/java/com/telerikacademy/testframework/pages/RegistrationPage.java
+++ TestAutomationFramework/src/main/java/com/telerikacademy/testframework/pages/RegistrationPage.java
@@ -14,96 +14,76 @@ public class RegistrationPage extends BasePage {
     }
 
     public void fillRegisterForm(String username, String password) {
-        actions.waitForElementClickable(USERNAME_FIELD_PATH);
-        actions.typeValueInField(Utils.getConfigPropertyByKey(username), USERNAME_FIELD_PATH);
-        actions.waitForElementClickable(PASSWORD_FIELD_PATH);
-        actions.typeValueInField(Utils.getConfigPropertyByKey(password), PASSWORD_FIELD_PATH);
-        actions.waitForElementClickable(CONFIRM_PASSWORD_FIELD_PATH);
-        actions.typeValueInField(Utils.getConfigPropertyByKey(password), CONFIRM_PASSWORD_FIELD_PATH);
-        actions.waitForElementClickable(SIGN_UP_BUTTON_PATH);
-        actions.clickElement(SIGN_UP_BUTTON_PATH);
+        enterCredentials(USERNAME_FIELD_PATH, Utils.getConfigPropertyByKey(username));
+        enterCredentials(PASSWORD_FIELD_PATH, Utils.getConfigPropertyByKey(password));
+        enterCredentials(CONFIRM_PASSWORD_FIELD_PATH, Utils.getConfigPropertyByKey(password));
+        clickButton(SIGN_UP_BUTTON_PATH);
     }
 
     public void fillOutRegistrationFormWithSpaces() {
-        actions.waitForElementClickable(USERNAME_FIELD_PATH);
-        actions.clickElement(USERNAME_FIELD_PATH);
+        clickButton(USERNAME_FIELD_PATH);
         actions.pressKey(Keys.SPACE);
         actions.pressKey(Keys.SPACE);
         actions.pressKey(Keys.SPACE);
-        actions.waitForElementClickable(PASSWORD_FIELD_PATH);
-        actions.clickElement(PASSWORD_FIELD_PATH);
+        clickButton(PASSWORD_FIELD_PATH);
         actions.pressKey(Keys.SPACE);
         actions.pressKey(Keys.SPACE);
         actions.pressKey(Keys.SPACE);
-        actions.waitForElementClickable(CONFIRM_PASSWORD_FIELD_PATH);
-        actions.clickElement(CONFIRM_PASSWORD_FIELD_PATH);
+        clickButton(CONFIRM_PASSWORD_FIELD_PATH);
         actions.pressKey(Keys.SPACE);
         actions.pressKey(Keys.SPACE);
         actions.pressKey(Keys.SPACE);
-        actions.waitForElementClickable(SIGN_UP_BUTTON_PATH);
-        actions.clickElement(SIGN_UP_BUTTON_PATH);
+        clickButton(SIGN_UP_BUTTON_PATH);
     }
 
     public void fillOutRegistrationFormWithMismatchPasswords(String username, String password, String confirmPassword) {
-        actions.typeValueInField(Utils.getConfigPropertyByKey(username), USERNAME_FIELD_PATH);
-        actions.typeValueInField(Utils.getConfigPropertyByKey(password), PASSWORD_FIELD_PATH);
-        actions.typeValueInField(Utils.getConfigPropertyByKey(confirmPassword), CONFIRM_PASSWORD_FIELD_PATH);
-        actions.waitForElementClickable(SIGN_UP_BUTTON_PATH);
-        actions.clickElement(SIGN_UP_BUTTON_PATH);
+        enterCredentials(USERNAME_FIELD_PATH, Utils.getConfigPropertyByKey(username));
+        enterCredentials(PASSWORD_FIELD_PATH, Utils.getConfigPropertyByKey(password));
+        enterCredentials(CONFIRM_PASSWORD_FIELD_PATH, Utils.getConfigPropertyByKey(confirmPassword));
+        clickButton(SIGN_UP_BUTTON_PATH);
     }
 
     public void navigateToHomePage() {
-        actions.waitForElementClickable(HOME_BUTTON_PATH);
-        actions.clickElement(HOME_BUTTON_PATH);
+        clickButton(HOME_BUTTON_PATH);
     }
 
     public void assertWelcomeMessageIsPresent() {
-        actions.waitForElementPresent(WELCOME_MESSAGE_PATH);
-        actions.assertElementPresent(WELCOME_MESSAGE_PATH);
+        assertElementPresent(WELCOME_MESSAGE_PATH);
     }
 
     public void assertEditProfileButtonIsPresent() {
-        actions.waitForElementPresent(EDIT_PROFILE_BUTTON);
-        actions.assertElementPresent(EDIT_PROFILE_BUTTON);
+        assertElementPresent(EDIT_PROFILE_BUTTON);
     }
 
     public void assertEmptyEmailErrorMessageIsPresent() {
-        actions.waitForElementPresent(EMPTY_EMAIL_ERROR_MESSAGE);
-        actions.assertElementPresent(EMPTY_EMAIL_ERROR_MESSAGE);
+        assertElementPresent(EMPTY_EMAIL_ERROR_MESSAGE);
     }
 
     public void assertEmptyPasswordErrorMessageIsPresent() {
-        actions.waitForElementPresent(EMPTY_PASSWORD_ERROR_MESSAGE);
-        actions.assertElementPresent(EMPTY_PASSWORD_ERROR_MESSAGE);
+        assertElementPresent(EMPTY_PASSWORD_ERROR_MESSAGE);
     }
 
     public void assertCreateNewAccountTextIsPresent() {
-        actions.waitForElementPresent(CREATE_NEW_ACCOUNT_TEXT);
-        actions.assertElementPresent(CREATE_NEW_ACCOUNT_TEXT);
+        assertElementPresent(CREATE_NEW_ACCOUNT_TEXT);
     }
 
     public void assertMinimumPasswordErrorMessageIsDisplayed() {
-        actions.waitForElementPresent(MINIMUM_PASSWORD_ERROR_MESSAGE);
-        actions.assertElementPresent(MINIMUM_PASSWORD_ERROR_MESSAGE);
+        assertElementPresent(MINIMUM_PASSWORD_ERROR_MESSAGE);
     }
 
     public void assertMaximumPasswordErrorMessageIsDisplayed() {
-        actions.waitForElementPresent(MAXIMUM_PASSWORD_ERROR_MESSAGE);
-        actions.assertElementPresent(MAXIMUM_PASSWORD_ERROR_MESSAGE);
+        assertElementPresent(MAXIMUM_PASSWORD_ERROR_MESSAGE);
     }
 
     public void assertSignUpButtonIsPresent() {
-        actions.waitForElementPresent(SIGN_UP_BUTTON_PATH);
-        actions.assertElementPresent(SIGN_UP_BUTTON_PATH);
+        assertElementPresent(SIGN_UP_BUTTON_PATH);
     }
 
     public void assertMismatchPasswordMessageIsPresent() {
-        actions.waitForElementPresent(MISMATCH_PASSWORD_ERROR_MESSAGE);
-        actions.assertElementPresent(MISMATCH_PASSWORD_ERROR_MESSAGE);
+        assertElementPresent(MISMATCH_PASSWORD_ERROR_MESSAGE);
     }
 
     public void assertEmailIsAlreadyTakenErrorMessageIsPresent() {
-        actions.waitForElementPresent(EMAIL_ALREADY_TAKEN_ERROR_MESSAGE);
-        actions.assertElementPresent(EMAIL_ALREADY_TAKEN_ERROR_MESSAGE);
+        assertElementPresent(EMAIL_ALREADY_TAKEN_ERROR_MESSAGE);
     }
 }
