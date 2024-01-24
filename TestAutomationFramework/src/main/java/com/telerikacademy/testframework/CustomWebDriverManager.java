fa708aca43b9aec25bdc8aa76507b6c3fcef0a75
Yordan Nikolov <x-normalize@protonmail.com>
2024-01-24 13:28:52 +0200

    Remove unused imports in CustomWebDriverManager.

--- TestAutomationFramework/src/main/java/com/telerikacademy/testframework/CustomWebDriverManager.java
+++ TestAutomationFramework/src/main/java/com/telerikacademy/testframework/CustomWebDriverManager.java
@@ -2,7 +2,6 @@ package com.telerikacademy.testframework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CustomWebDriverManager {
 
